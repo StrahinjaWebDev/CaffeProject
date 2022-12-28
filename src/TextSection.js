f90da894ab171889e9dc62498a2666810fe92e4d
Strahinja <vujinovicstrahinja123@gmail.com>
2022-12-28 21:28:46 +0100

    first commit

---
+++ src/TextSection.js
@@ -0,0 +1,17 @@
+import React from "react"
+export default TextSection;
+
+
+function TextSection()  {
+
+  return (
+    <div>
+      <div className="TextSectionMain">
+        <h1>Meet, Eat & Enjoy the True Taste From Smart Meals.</h1>
+      </div>
+      <div className="TextSectionSecond">
+        <p>aaa</p>
+      </div>
+    </div>
+  );
+}
